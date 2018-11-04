de7054b63a2671d318e81f5eba0261c643e10f2b
new4 <new4wang@163.com>
2018-11-04 17:11:06 +0800

    update pkg

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,12 +6,8 @@ module.exports = {
   'extends': ['new4-eslintrc'],
   'rules': {
     'no-console': 'off',
-    'no-param-reassign': 'off',
     // 禁止在返回语句中赋值
     'no-return-assign': 'off',
-    // 禁用一元操作符 ++ 和 --
-    'no-plusplus': ['off'],
-
     'no-underscore-dangle': ["off"]
   },
   'parserOptions': {
