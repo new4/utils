14e737911cb34cdfab8f0214b42ba45b62a4e93c
new4 <new4wang@163.com>
2018-11-05 22:52:03 +0800

    add cache in getNodeModulesDir

--- utils/getNodeModulesDir.js
+++ utils/getNodeModulesDir.js
@@ -9,11 +9,18 @@ const {
   yellow,
 } = require('./colorStr');
 
+let cacheDir;
+
 module.exports = () => {
+  if (cacheDir) {
+    return cacheDir;
+  }
+
   let layer = 10; // 限制从当前目录向上找 10 层
   let curDir = __dirname;
   while (layer--) {
     if (fs.existsSync(path.join(curDir, 'node_modules'))) {
+      cacheDir = curDir;
       return curDir;
     }
     curDir = path.join(curDir, '..');
