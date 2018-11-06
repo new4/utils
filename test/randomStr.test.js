683d1ae7e9638658360cd6936c84e0dde664bef9
new4 <new4wang@163.com>
2018-11-06 18:14:39 +0800

    add randomStr.test.js

---
+++ test/randomStr.test.js
@@ -0,0 +1,25 @@
+const { expect } = require('chai');
+
+const randomStr = require('../lib/randomStr');
+
+describe('randomStr', function () {
+  it('randomStr().length => 7', function () {
+    expect(randomStr().length).to.equal(7);
+  });
+
+  it('randomStr(2).length => 2', function () {
+    expect(randomStr(2).length).to.equal(2);
+  });
+
+  it('/[_0-9a-zA-Z]+/.test(randomStr()) => true', function () {
+    expect(/[_0-9a-zA-Z]+/.test(randomStr())).to.equal(true);
+  });
+
+  it('/[_0-9a-zA-Z]{7}/.test(randomStr(7)) => true', function () {
+    expect(/[_0-9a-zA-Z]{7}/.test(randomStr(7))).to.equal(true);
+  });
+
+  it('/[_0-9a-zA-Z]{17}/.test(randomStr(17)) => true', function () {
+    expect(/[_0-9a-zA-Z]{17}/.test(randomStr(17))).to.equal(true);
+  });
+});
