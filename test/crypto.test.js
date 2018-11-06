a739aba080406b247c53d79f33c02fcbade2100a
new4 <new4wang@163.com>
2018-11-06 18:20:59 +0800

    add crypto.test.js

---
+++ test/crypto.test.js
@@ -0,0 +1,9 @@
+const { expect } = require('chai');
+const { encrypt, decrypt } = require('../lib/crypto');
+const randomStr = require('../lib/randomStr')();
+
+describe('crypto', function () {
+  it('decrypt(encrypt(randomStr)) => randomStr', function () {
+    expect(decrypt(encrypt(randomStr))).to.equal(randomStr);
+  });
+});
