b79004fb48bbe81b76b3071e48e13329d14988a5
new4 <new4wang@163.com>
2018-11-06 17:23:50 +0800

    add itWrapper

--- test/addonZero.test.js
+++ test/addonZero.test.js
@@ -2,6 +2,14 @@ const { expect } = require('chai');
 
 const addonZero = require('../lib/addonZero');
 
+function itWrapper(args, expected) {
+  const serializedArgs = JSON.stringify(args).replace(/(\[|\])/g, '').replace(',', ', ');
+  return it(
+    `addonZero(${serializedArgs}) => ${expected}`,
+    () => expect(addonZero(...args)).to.equal(expected),
+  );
+}
+
 describe('addonZero', function () {
   describe('不传参数', function () {
     it('addonZero() => 000', function () {
@@ -10,44 +18,61 @@ describe('addonZero', function () {
   });
 
   describe('传一个参数', function () {
-    it('addonZero(0) => 000', function () {
-      expect(addonZero(0)).to.equal('000');
-    });
-
-    it('addonZero(1) => 001', function () {
-      expect(addonZero(1)).to.equal('001');
-    });
-
-    it('addonZero(12) => 012', function () {
-      expect(addonZero(12)).to.equal('012');
-    });
-
-    it('addonZero("12") => 012', function () {
-      expect(addonZero('12')).to.equal('012');
-    });
+    [
+      {
+        args: [0],
+        expected: '000',
+      },
+      {
+        args: [1],
+        expected: '001',
+      },
+      {
+        args: [12],
+        expected: '012',
+      },
+      {
+        args: ['ad'],
+        expected: '0ad',
+      },
+    ].forEach(({ args, expected }) => itWrapper(args, expected));
   });
 
   describe('传两个参数', function () {
-    it('addonZero("12", 1) => 12', function () {
-      expect(addonZero('12', 1)).to.equal('12');
-    });
-    it('addonZero("12", 2) => 12', function () {
-      expect(addonZero('12', 2)).to.equal('12');
-    });
-    it('addonZero("12", 3) => 012', function () {
-      expect(addonZero('12', 3)).to.equal('012');
-    });
-    it('addonZero("12", 5) => 00012', function () {
-      expect(addonZero('12', 5)).to.equal('00012');
-    });
-    it('addonZero("12", 10) => 00012', function () {
-      expect(addonZero('12', 10)).to.equal('0000000012');
-    });
+    [
+      {
+        args: ['12', 1],
+        expected: '12',
+      },
+      {
+        args: ['12', 2],
+        expected: '12',
+      },
+      {
+        args: ['12', 3],
+        expected: '012',
+      },
+      {
+        args: ['12', 5],
+        expected: '00012',
+      },
+      {
+        args: ['12', 10],
+        expected: '0000000012',
+      },
+    ].forEach(({ args, expected }) => itWrapper(args, expected));
   });
 
   describe('传三个参数', function () {
-    it('addonZero("1", 5,"$") => $$$$1', function () {
-      expect(addonZero('1', 5, '$')).to.equal('$$$$1');
-    });
+    [
+      {
+        args: ['1', 5, '$'],
+        expected: '$$$$1',
+      },
+      {
+        args: ['1234', 5, '$'],
+        expected: '$1234',
+      },
+    ].forEach(({ args, expected }) => itWrapper(args, expected));
   });
 });
